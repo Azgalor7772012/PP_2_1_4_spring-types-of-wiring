82064be654dfb802b3729a80b5a68609b141f430
Schurov Konstantin <shurovkp@gmail.com>
2022-10-28 11:36:25 +0300

    myCommit

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,7 +1,6 @@
 package koschei.config;
 
-import koschei.models.Island2;
-import koschei.models.Wood3;
+import koschei.models.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +9,27 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
 
-    @Bean
+    @Bean(name = "island")
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+
+    @Bean
+    public static Egg6 getEgg6(Needle7 needle) {
+        return new Egg6(needle);
+    }
+
+    @Bean
+    public static Needle7 needle7(Deth8 deth8) {
+        Needle7 needle7 = new Needle7();
+        needle7.setDeth(deth8);
+        return needle7;
+    }
+
+    @Bean
+    public static Deth8 getDeth8() {
+        return new Deth8();
+    }
+
+
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -2,8 +2,15 @@ package koschei.models;
 
 public class Needle7 {
 
+    private Deth8 deth8;
+
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
+    }
+
+
+    public void setDeth(Deth8 deth8) {
+        this.deth8 = deth8;
     }
 }
